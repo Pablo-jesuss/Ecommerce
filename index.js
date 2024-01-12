97301c68438c6864bb14b037037cc1f731e411fe
Pablo-jesuss <pablo.programador26@gmail.com>
2024-01-12 16:39:05 -0300

    removeCart

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ if (document.readyState == "loading") {
 } else {
   ready();
 }
+
+// Function
+
+function ready() {
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+}
