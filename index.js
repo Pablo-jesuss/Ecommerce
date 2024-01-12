663672f415b3f5fac49e8cce6288dabc8737f24f
Pablo-jesuss <pablo.programador26@gmail.com>
2024-01-12 16:25:34 -0300

    loading

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
-
+// Open
 cartIcon.onclick = () => {
   cart.classList.add("active");
 };
-
+// Close
 closeCart.onclick = () => {
   cart.classList.remove("active");
 };
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
