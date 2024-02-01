ff0a0e5de2809a897493c3c78c6f863cd339861e
Pablo-jesuss <pablo.programador26@gmail.com>
2024-02-01 18:54:50 -0300

    add cart

--- index.js
+++ index.js
@@ -44,6 +44,26 @@ for (var i = 0; i < addCart.length; i++) {
   button.addEventListener("click", addCartClicked);
 }
 
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price = shopProducts.getElementsByClassName("price")[0].innerText;
+  var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  addProductToCart(title, price, productImg);
+  updatetotal();
+}
+
+function addProductToCart(title, price, productImg) {
+  var cartShopBox = document.createElement("div");
+  // cartShopBox.classList.add('cart-box')
+  var cartItems = document.getElementsByClassName("cart-content")[0];
+  var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    alert("Você já adicionou este item ao carrinho");
+  }
+}
+
 // Remove cart
 
 function removeCartItem(event) {
@@ -64,20 +84,23 @@ function quantityChanged(event) {
 
 function updatetotal() {
   var cartContent = document.getElementsByClassName("cart-content")[0];
-  var cartBoxes = cartContent.getElementsByClassName("cart-box"); // Correção aqui
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
   var total = 0;
 
   for (var i = 0; i < cartBoxes.length; i++) {
     var cartBox = cartBoxes[i];
     var priceElement = cartBox.getElementsByClassName("cart-price")[0];
     var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
-    var price = parseFloat(priceElement.innerText.replace("R$", ""));
+    var price = parseFloat(
+      priceElement.innerText.replace("R$", "").replace(",", ".")
+    );
     var quantity = parseFloat(quantityElement.value);
 
     total = total + price * quantity;
-    total = Math.round(total * 100) / 100;
   }
 
+  total = Math.round(total * 100) / 100;
+
   document.getElementsByClassName("total-price")[0].innerText =
-    "R$" + total.toFixed(2); // Adicionado toFixed(2) para formatar o total com duas casas decimais
+    "R$" + total.toFixed(2);
 }
