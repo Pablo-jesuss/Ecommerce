62cdba0f7249e0369eb34db3b4b1d61df732a8be
Pablo-jesuss <pablo.programador26@gmail.com>
2024-02-02 17:09:02 -0300

    commit

--- index.js
+++ index.js
@@ -55,31 +55,39 @@ function addCartClicked(event) {
 }
 
 function addProductToCart(title, price, productImg) {
-  var cartShopBox = document.createElement("div");
-  // cartShopBox.classList.add('cart-box')
   var cartItems = document.getElementsByClassName("cart-content")[0];
   var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
-  for (var i = 0; i < cartItemsNames.length; i++) {
-    alert("Você já adicionou este item ao carrinho");
-  }
-}
 
-var cartBoxContent = `<img src="image/product2.jpg" alt="" class="cart-img" />
-<div class="detail-box">
-  <div class="cart-product-title">Fone via bluetooth</div>
-  <div class="cart-price">R$ 300,00</div>
-  <input type="number" value="1" class="cart-quantity" />
-</div>
-<!--Remove Cart-->
-<i class="bx bxs-trash-alt cart-remove"></i>`;
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox);
-cartShopBox
-  .getElementsByClassName("cart-remove")[0]
-  .addEventListener("click", removeCartItem);
-cartShopBox
-  .getElementsByClassName("cart-quantity")[0]
-  .addEventListener("change", quantityChanged);
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].textContent === title) {
+      alert("Você já adicionou este item ao carrinho");
+      return;
+    }
+  }
+
+  var cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+
+  var cartBoxContent = `<img src="${productImg}" alt="" class="cart-img" />
+    <div class="detail-box">
+      <div class="cart-product-title">${title}</div>
+      <div class="cart-price">${price}</div>
+      <input type="number" value="1" class="cart-quantity" />
+    </div>
+    <!--Remove Cart-->
+    <i class="bx bxs-trash-alt cart-remove"></i>`;
+
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+
+  cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+
+  cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantityChanged);
+}
 
 // Remove cart
 
